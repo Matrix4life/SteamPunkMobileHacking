fecc49aeaad718a95acb122731e68623df230269
Matrix4life <websiteflipper4life@gmail.com>
2026-04-07 14:41:09 -0400

    Update vite.config.js

--- steambreach_mobile_v3/work/vite.config.js
+++ steambreach_mobile_v3/work/vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      
+      // Tell the plugin to inject the crossorigin attribute
+      useCredentials: true, 
+      
+      manifest: {
+        name: 'Syntax Syndicate',
+        short_name: 'Syntax',
+        description: 'Cyber-noir hacking terminal',
+        theme_color: '#05080c',
+        icons: [
+          // ... your icons ...
+        ]
+      }
+    })
+  ],
 })
